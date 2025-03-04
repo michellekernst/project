a5e6e260bd3c607482d12991c8a062114b0ee3a1
michellekernst <mke.ernst@gmail.com>
2025-03-04 16:57:06 -0600

    simplified code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let playerHP = 250;
 let enemyHP = 250;
 
 let responseText = document.getElementById("response");
-console.log(responseText);
 let enemyHPDisplay = document.getElementById("enemyHPDisplay");
 let playerHPDisplay = document.getElementById("playerHPDisplay");
 let enemyResponse = document.getElementById("enemyresponse");
@@ -19,78 +18,74 @@ function resetShield() {
   enemyShieldDisplay.textContent = "Enemy shield: " + shield;
 }
 
-function unarmedStrike() {
+function ifShielded() {
+  if (enemyShield > 0) {
+    responseText.textContent += ` It was shielded for ${enemyShield} damage, doing a total of ${damage} damage.`;
+  }
+}
+
+function checkEnemyDead() {
+  if (enemyHP === 0) {
+    responseText.textContent = "Veneficus Malus died! You win!";
+    enemyResponse.textContent = "";
+  } else {
+    enemyAttack();
+  }
+}
+
+// Player attacks //
+function playerAttack(attackType) {
   resetShield();
   if (playerHP === 0) {
     responseText.textContent = "You are dead.";
   } else {
-    let damageUnshielded = Math.floor(Math.random() * 5) + 1;
+    let minDamage, maxDamage, attackName;
+    switch (attackType) {
+      case "unarmedStrike":
+        minDamage = 1;
+        maxDamage = 5;
+        attackName = "struck Veneficus Malus with your fist";
+        break;
+      case "fireball":
+        minDamage = 20;
+        maxDamage = 40;
+        attackName = "cast fireball";
+        break;
+      case "thunderbolt":
+        minDamage = 5;
+        maxDamage = 50;
+        attackName = "cast thunderbolt";
+        break;
+    }
+    let damageUnshielded =
+      Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
     let damage = Math.max(0, damageUnshielded - enemyShield);
     enemyHP = Math.max(0, enemyHP - damage);
-    responseText.textContent = `You struck Veneficus Malus with your fist for ${damageUnshielded} damage.`;
-    if (enemyShield > 0) {
-      responseText.textContent += ` It was shielded for ${enemyShield} damage, doing a total of ${damage} damage.`;
-    }
+    responseText.textContent = `You ${attackName} for ${damageUnshielded} damage.`;
+
+    ifShielded();
+
     enemyHPDisplay.textContent = "HP: " + enemyHP;
-    if (enemyHP === 0) {
-      responseText.textContent = "Veneficus Malus died! You win!";
-      enemyResponse.textContent = "";
-    } else {
-      enemyAttack();
-    }
+
+    checkEnemyDead();
   }
+}
+
+function unarmedStrike() {
+  playerAttack("unarmedStrike");
 }
 
 function castFireball() {
-  resetShield();
-  if (playerHP === 0) {
-    responseText.textContent = "You are dead.";
-  } else {
-    let minDamage = 20;
-    let damageUnshielded =
-      Math.floor(Math.random() * (40 - minDamage + 1)) + minDamage;
-    let damage = Math.max(0, damageUnshielded - enemyShield);
-    enemyHP = Math.max(0, enemyHP - damage);
-    responseText.textContent = `You cast fireball for ${damageUnshielded} damage.`;
-    if (enemyShield > 0) {
-      responseText.textContent += ` It was shielded for ${enemyShield} damage, doing a total of ${damage} damage.`;
-    }
-    enemyHPDisplay.textContent = "HP: " + enemyHP;
-    if (enemyHP === 0) {
-      responseText.textContent = "Veneficus Malus died! You win!";
-      enemyResponse.textContent = "";
-    } else {
-      enemyAttack();
-    }
-  }
+  playerAttack("fireball");
 }
 
 function castThunderbolt() {
-  resetShield();
-  if (playerHP === 0) {
-    responseText.textContent = "You are dead.";
-  } else {
-    let minDamage = 5;
-    let damageUnshielded =
-      Math.floor(Math.random() * (50 - minDamage + 1)) + minDamage;
-    let damage = Math.max(0, damageUnshielded - enemyShield);
-    enemyHP = Math.max(0, enemyHP - damage);
-    responseText.textContent = `You cast thunderbolt for ${damageUnshielded} damage.`;
-    if (enemyShield > 0) {
-      responseText.textContent += ` It was shielded for ${enemyShield} damage, doing a total of ${damage} damage.`;
-    }
-    enemyHPDisplay.textContent = "HP: " + enemyHP;
-    if (enemyHP === 0) {
-      responseText.textContent = "Veneficus Malus died! You win!";
-      enemyResponse.textContent = "";
-    } else {
-      enemyAttack();
-    }
-  }
+  playerAttack("thunderbolt");
 }
 
 function castShield() {
   resetShield();
+
   if (playerHP === 0) {
     responseText.textContent = "You are dead.";
   } else {
@@ -100,43 +95,27 @@ function castShield() {
       "You protected yourself from " + shield + " future damage.";
     responseText.textContent = "You cast shield." + " " + shieldOutput;
     playerShieldDisplay.textContent = "Player shield: " + shield;
-    if (enemyHP === 0) {
-      responseText.textContent = "Veneficus Malus died! You win!";
-      enemyResponse.textContent = "";
-    } else {
-      enemyAttack();
-    }
+
+    checkEnemyDead();
   }
 }
 
 function enemyAttack() {
   let attack = Math.floor(Math.random() * 3);
+  let minDamage, maxDamage, attackName;
 
   if (attack === 0) {
-    let minDamage = 25;
-    let enemyDamageUnshielded =
-      Math.floor(Math.random() * (50 - minDamage + 1)) + minDamage;
-    let enemyDamage = Math.max(0, enemyDamageUnshielded - shield);
-    playerHP = Math.max(0, playerHP - enemyDamage);
-    enemyResponse.textContent = `Veneficus Malus cast fireball for ${enemyDamageUnshielded} damage.`;
-    if (shield > 0) {
-      enemyResponse.textContent += ` It was shielded for ${shield} damage, doing a total of ${enemyDamage}.`;
-    }
-    playerHPDisplay.textContent = "HP: " + playerHP;
+    // fireball
+    minDamage = 25;
+    maxDamage = 50;
+    attackName = "cast fireball";
   } else if (attack === 1) {
-    let minDamage = 5;
-    let enemyDamageUnshielded =
-      Math.floor(Math.random() * (35 - minDamage + 1)) + minDamage;
-    let enemyDamage = Math.max(0, enemyDamageUnshielded - shield);
-    playerHP = Math.max(0, playerHP - enemyDamage);
-    enemyResponse.textContent = `Veneficus Malus cast thunderbolt for ${enemyDamageUnshielded} damage.`;
-    if (shield > 0) {
-      if (shield > 0) {
-        enemyResponse.textContent += ` It was shielded for ${shield} damage, doing a total of ${enemyDamage}.`;
-      }
-    }
-    playerHPDisplay.textContent = "HP: " + playerHP;
+    // thunderbolt
+    minDamage = 5;
+    maxDamage = 35;
+    attackName = "cast thunderbolt";
   } else {
+    // shield
     let minShield = 5;
     enemyShield = Math.floor(Math.random() * (50 - minShield + 1)) + minShield;
     let enemyShieldOutput =
@@ -144,7 +123,20 @@ function enemyAttack() {
     enemyResponse.textContent =
       "Veneficus Malus cast shield." + " " + enemyShieldOutput;
     enemyShieldDisplay.textContent = "Enemy shield: " + enemyShield;
+    return;
   }
+
+  let enemyDamageUnshielded =
+    Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+  let enemyDamage = Math.max(0, enemyDamageUnshielded - shield);
+  playerHP = Math.max(0, playerHP - enemyDamage);
+  enemyResponse.textContent = `Veneficus Malus ${attackName} for ${enemyDamageUnshielded} damage.`;
+
+  if (shield > 0) {
+    enemyResponse.textContent += ` It was shielded for ${shield} damage, doing a total of ${enemyDamage}.`;
+  }
+
+  playerHPDisplay.textContent = "HP: " + playerHP;
 
   if (playerHP === 0) {
     responseText.textContent = "You died!";
