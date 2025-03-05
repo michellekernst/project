7c7d9a15d40b190d91048e17ba03a3366b81f8fd
michellekernst <mke.ernst@gmail.com>
2025-03-04 20:37:55 -0600

    aesthetic changes

--- index.js
+++ index.js
@@ -5,69 +5,80 @@ let responseText = document.getElementById("response");
 let enemyHPDisplay = document.getElementById("enemyHPDisplay");
 let playerHPDisplay = document.getElementById("playerHPDisplay");
 let enemyResponse = document.getElementById("enemyresponse");
-let enemyShieldDisplay = document.getElementById("enemyshielddisplay");
-let playerShieldDisplay = document.getElementById("playershielddisplay");
+let playerImg = document.getElementById("player-img");
+let enemyImg = document.getElementById("enemy-img");
+let stats = document.getElementById("stats");
 
 let shield = 0;
 let enemyShield = 0;
-
-function resetShield() {
-  shield = 0;
-  enemyShield = 0;
-  playerShieldDisplay.textContent = "Your shield: " + shield;
-  enemyShieldDisplay.textContent = "Enemy shield: " + shield;
-}
-
-function ifShielded() {
-  if (enemyShield > 0) {
-    responseText.textContent += ` It was shielded for ${enemyShield} damage, doing a total of ${damage} damage.`;
-  }
-}
 
 function checkEnemyDead() {
   if (enemyHP === 0) {
     responseText.textContent = "Veneficus Malus died! You win!";
     enemyResponse.textContent = "";
+    enemyImg.src = "enemy/dead.gif";
   } else {
     enemyAttack();
   }
 }
 
-// Player attacks //
-function playerAttack(attackType) {
-  resetShield();
+function checkPlayerDead() {
   if (playerHP === 0) {
-    responseText.textContent = "You are dead.";
+    responseText.textContent = "You died!";
+    enemyResponse.textContent = "";
+    playerImg.src = "player/dead.gif";
+  }
+}
+
+// player attacks //
+function playerAttack(attackType) {
+  if (playerHP === 0) {
+    responseText.textContent = "You are dead."; // cannot attack if dead
   } else {
     let minDamage, maxDamage, attackName;
     switch (attackType) {
       case "unarmedStrike":
         minDamage = 1;
         maxDamage = 5;
-        attackName = "struck Veneficus Malus with your fist";
+        attackName = "struck Veneficus Malus";
+        playerImg.src = "player/strike.gif";
         break;
       case "fireball":
         minDamage = 20;
         maxDamage = 40;
         attackName = "cast fireball";
+        playerImg.src = "player/fireball.gif";
         break;
       case "thunderbolt":
         minDamage = 5;
         maxDamage = 50;
         attackName = "cast thunderbolt";
+        playerImg.src = "player/lightning.gif";
         break;
     }
+
     let damageUnshielded =
-      Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
-    let damage = Math.max(0, damageUnshielded - enemyShield);
-    enemyHP = Math.max(0, enemyHP - damage);
+      Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage; // player damage before any shields
+    let damage = Math.max(0, damageUnshielded - enemyShield); // player damage after shields, no less than 0
+    enemyHP = Math.max(0, enemyHP - damage); // enemy HP after attack, no less than 0
+
     responseText.textContent = `You ${attackName} for ${damageUnshielded} damage.`;
 
-    ifShielded();
+    if (enemyShield > 0) {
+      responseText.textContent += ` It was shielded for ${enemyShield} damage, doing a total of ${damage} damage.`;
+      enemyShield = 0;
+    }
 
-    enemyHPDisplay.textContent = "HP: " + enemyHP;
+    enemyHPDisplay.textContent = enemyHP + " / 250";
+    if (enemyHP < 62) {
+      enemyHPDisplay.style.backgroundColor = "red";
+    } else if (playerHP < 125) {
+      enemyHPDisplay.style.backgroundColor = "orange";
+    } else if (playerHP < 187) {
+      enemyHPDisplay.style.backgroundColor = "yellow";
+    }
 
-    checkEnemyDead();
+    checkEnemyDead(); // if enemy has 0 HP, shows enemy dead
   }
 }
 
@@ -84,45 +95,46 @@ function castThunderbolt() {
 }
 
 function castShield() {
-  resetShield();
-
   if (playerHP === 0) {
-    responseText.textContent = "You are dead.";
+    responseText.textContent = "You are dead."; // cannot shield if dead
+    shield = 0;
   } else {
+    playerImg.src = "player/idle.gif";
     let minShield = 5;
     shield = Math.floor(Math.random() * (50 - minShield + 1)) + minShield;
     let shieldOutput =
       "You protected yourself from " + shield + " future damage.";
     responseText.textContent = "You cast shield." + " " + shieldOutput;
-    playerShieldDisplay.textContent = "Player shield: " + shield;
-
     checkEnemyDead();
   }
 }
 
+// enemy attacks //
 function enemyAttack() {
   let attack = Math.floor(Math.random() * 3);
   let minDamage, maxDamage, attackName;
 
   if (attack === 0) {
     // fireball
+    enemyImg.src = "enemy/strike.gif";
     minDamage = 25;
     maxDamage = 50;
     attackName = "cast fireball";
   } else if (attack === 1) {
     // thunderbolt
+    enemyImg.src = "enemy/attack.gif";
     minDamage = 5;
     maxDamage = 35;
     attackName = "cast thunderbolt";
   } else {
     // shield
+    enemyImg.src = "enemy/sphere.gif";
     let minShield = 5;
     enemyShield = Math.floor(Math.random() * (50 - minShield + 1)) + minShield;
     let enemyShieldOutput =
       "He protected himself from " + enemyShield + " future damage.";
     enemyResponse.textContent =
       "Veneficus Malus cast shield." + " " + enemyShieldOutput;
-    enemyShieldDisplay.textContent = "Enemy shield: " + enemyShield;
     return;
   }
 
@@ -133,13 +145,18 @@ function enemyAttack() {
   enemyResponse.textContent = `Veneficus Malus ${attackName} for ${enemyDamageUnshielded} damage.`;
 
   if (shield > 0) {
-    enemyResponse.textContent += ` It was shielded for ${shield} damage, doing a total of ${enemyDamage}.`;
+    enemyResponse.textContent += ` It was shielded for ${shield} damage, doing a total of ${enemyDamage} damage.`;
+    shield = 0; // reset shield after attack
   }
 
-  playerHPDisplay.textContent = "HP: " + playerHP;
-
-  if (playerHP === 0) {
-    responseText.textContent = "You died!";
-    enemyResponse.textContent = "";
+  playerHPDisplay.textContent = playerHP + " / 250";
+  if (playerHP < 62) {
+    playerHPDisplay.style.backgroundColor = "red";
+  } else if (playerHP < 125) {
+    playerHPDisplay.style.backgroundColor = "orange";
+  } else if (playerHP < 187) {
+    playerHPDisplay.style.backgroundColor = "yellow";
   }
+
+  checkPlayerDead();
 }
